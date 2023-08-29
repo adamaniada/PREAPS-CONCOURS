85698254917a2eca917b52f56227c6a083ad0b1c
ADAMA NIADA <aniada1mg@gmail.com>
2023-08-29 10:45:14 +0100

    Update layouts/app.blade.php file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,11 +98,15 @@
             }
         </style>
 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
 
         <!-- Custom styles for this template -->
         <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="{{ asset('css/blog.css') }}" rel="stylesheet">
+
+        @stack('styles') <!-- Inclure les styles ici -->
     </head>
     <body>
         <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
@@ -179,6 +183,13 @@
         </main>
 
         @include('layouts.include.app.footer')
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="http://ff.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=JtAz3I6efqiPHP43xyBHgmxGsBof_ess_EOHqBh4dzNLQfUZni3Otzz1jFfRjuaDqxCx_f-ZrkrEJSnUfKrenWBQqtiFJzgzZ68eNMS1bxLDqDH26O3M0WUEJaa5puDt" charset="UTF-8"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+        <!-- Inclure les scripts ici -->
+        @stack('scripts')
 
     </body>
 </html>
